7aab0f3fee9f6705f60bc29c62189284b6d63109
Opeyemi Ibrahim <sirkhadrealryan@gmail.com>
2018-03-08 15:46:42 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -8,36 +8,16 @@ Applicants may use whatever language they want to achieve the outcome.
 
 ## Task
 
-Build a bot that extracts the following from people’s Twitter bio (on public/open accounts), into a Google spreadsheet:
+Built a bot that extracts the following from people’s Twitter bio (on public/open accounts), into a Google spreadsheet:
 
 * Twitter profile name 
 * Number of followers
 
-Target accounts using either of these criteria:
+Target accounts using this criteria:
 * Based on hashtags used
-* Based on number of followers; Between 1,000 - 50,000
 
-The bot is suppose to maintain a session and continously listen to the predefined hashtag
-
-## How to complete the task
-
-1. Fork this repository into your own public repo.
-
-2. Complete the project and commit your work. Send the URL of your own repository to @seun on the Slack here bit.ly/dcs-slack.
 
 ## Show your working
 
 If you choose to use build tools to compile your CSS and Javascript (such as SASS of Coffescript) please include the original files as well. You may update this README file outlining the details of what tools you have used.
-
-## Clean code
-
-This fictitious project is part of a larger plan to reuse templates for multiple properties. When authoring your CSS ensure that it is easy for another developer to find and change things such as fonts and colours.
-
-
-## Good luck!
-
-We look forward to seeing what you can do. Remember, although it is a test, there are no specific right or wrong answers that we are looking for - just do the job as best you can. Any questions - create an issue in the panel on the right (requires a Github account).
-
-
-## Demo
-![screen shot](https://user-images.githubusercontent.com/8668661/33088863-330b4250-ceef-11e7-9e9c-b4fd9ca299d8.gif)
+This was built on PHP using abraham twiiter oauth and google client api 
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+use Google\Spreadsheet\DefaultServiceRequest;
+use Google\Spreadsheet\ServiceRequestFactory;
+
+
+ 	$aDiceValues = array();
+	$consumerKey = "5DRHOI34z4Eb2P5INeZnTn5dP";
+	$consumerSecret = "uH10YyynLJZIbMEQk4Zq42UHSJBmXvbhT98Kmr8yRaHBJON6AU";
+	$accessToken = "269931982-lvZn8v0Uf8hW9UKl2SqRFQesvWeqrb6knreHSgzw";
+	$accessTokenSecret = "1UGNYBESwuTHxeLgCVU8QAqdWxLpYLLqRHO2EQZuIAPUK" ;
+
+
+	$connection =  new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+
+	putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/client_secret.json');
+	$client = new Google_Client;
+	$client->useApplicationDefaultCredentials();
+	 
+	$client->setApplicationName("Something to do with my representatives");
+	$client->setScopes(['https://www.googleapis.com/auth/drive','https://spreadsheets.google.com/feeds']);
+	 
+	if ($client->isAccessTokenExpired()) {
+	    $client->refreshTokenWithAssertion();
+	}
+	 
+	$accessToken = $client->fetchAccessTokenWithAssertion()["access_token"];
+	ServiceRequestFactory::setInstance(
+	    new DefaultServiceRequest($accessToken)
+	);
+
+
+	$spreadsheetService = new Google\Spreadsheet\SpreadsheetService();
+	$spreadsheetFeed = $spreadsheetService->getSpreadsheetFeed();
+
+
+	$spreadsheet = $spreadsheetFeed->getByTitle('Twitter Bot');
+
+	$worksheets = $spreadsheet->getWorksheetFeed()->getEntries();
+
+	$worksheet = $worksheets[1];
+	$listFeed = $worksheet->getListFeed();
+	$cellFeed = $worksheet->getCellFeed();
+	$cellFeed->editCell(1,1, "Name");
+	$cellFeed->editCell(1,2, "Followers");
+
+	// Search for users with tweet that contains a certain hashtag
+	$data = $connection->get("users/search", ["q" => "%23Arsenal", "page" => 1, "count" => 20]);
+
+?>
+
+<html>
+	<head>
+		<title>Twitter Bot</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div class="container">
+			<table>
+				<thead>
+					<tr>
+						<td>SN</td>
+						<td>Profile Name</td>
+						<td>Number of Followers</td>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+						foreach ($data as $user)
+						{
+							
+							$listFeed->insert([
+						        'name' => $user->name,
+						        'followers'	=> $user->followers_count
+						    ]);
+
+							echo "<tr>";
+							echo "<td>".$user->name . " </td>" ;
+							echo "<td>" .number_format($user->followers_count, 0) ."</td>" ;
+							echo "</tr>";
+							
+						} 
+					?>
+				</tbody>
+			</table>
+		</div>
+</body>
+</html>
+
+
