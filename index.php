dd1355ae2202cf4ab0ca740d966caebc24ac8b2a
Opeyemi Ibrahim <sirkhadrealryan@gmail.com>
2018-03-20 23:16:16 +0100

    Add new library and code refactor

--- index.php
+++ index.php
@@ -1,87 +1,75 @@
 <?php
 
 require "vendor/autoload.php";
+require_once('OauthPhirehose.php');
 
-use Abraham\TwitterOAuth\TwitterOAuth;
 use Google\Spreadsheet\DefaultServiceRequest;
 use Google\Spreadsheet\ServiceRequestFactory;
-$response = new \League\CLImate\CLImate;
 
- 	$aDiceValues = array();
-	$consumerKey = "5DRHOI34z4Eb2P5INeZnTn5dP";
-	$consumerSecret = "uH10YyynLJZIbMEQk4Zq42UHSJBmXvbhT98Kmr8yRaHBJON6AU";
-	$accessToken = "269931982-lvZn8v0Uf8hW9UKl2SqRFQesvWeqrb6knreHSgzw";
-	$accessTokenSecret = "1UGNYBESwuTHxeLgCVU8QAqdWxLpYLLqRHO2EQZuIAPUK" ;
+ class ScrapTweet extends OauthPhirehose
+{
+  public function saveInPreadSheet($name, $followers)
+  {
+    putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/client_secret.json');
+    $client = new Google_Client;
+    $client->useApplicationDefaultCredentials();
+     
+    $client->setApplicationName("Devcenter Tweet Bot");
+    $client->setScopes(['https://www.googleapis.com/auth/drive','https://spreadsheets.google.com/feeds']);
+     
+    if ($client->isAccessTokenExpired()) {
+        $client->refreshTokenWithAssertion();
+    }
+     
+    $accessToken = $client->fetchAccessTokenWithAssertion()["access_token"];
+    ServiceRequestFactory::setInstance(
+        new DefaultServiceRequest($accessToken)
+    );
 
+    $spreadsheetService = new Google\Spreadsheet\SpreadsheetService();
+    $spreadsheetFeed = $spreadsheetService->getSpreadsheetFeed();
+    $spreadsheet = $spreadsheetFeed->getByTitle('Twitter Bot');
 
-	$connection =  new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+    $worksheets = $spreadsheet->getWorksheetFeed()->getEntries();
 
-	putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/client_secret.json');
-	$client = new Google_Client;
-	$client->useApplicationDefaultCredentials();
-	 
-	$client->setApplicationName("Devcenter Tweet Bot");
-	$client->setScopes(['https://www.googleapis.com/auth/drive','https://spreadsheets.google.com/feeds']);
-	 
-	if ($client->isAccessTokenExpired()) {
-	    $client->refreshTokenWithAssertion();
-	}
-	 
-	$accessToken = $client->fetchAccessTokenWithAssertion()["access_token"];
-	ServiceRequestFactory::setInstance(
-	    new DefaultServiceRequest($accessToken)
-	);
-	
-
-	
-		echo "Enter the hashtag below without the # sign :\n";
-	$var = fread(STDIN, 80); // Read up to 80 characters or a newline
-	$hashtag = "%23". $var;
-
-
-	$spreadsheetService = new Google\Spreadsheet\SpreadsheetService();
-	$spreadsheetFeed = $spreadsheetService->getSpreadsheetFeed();
+    $worksheet = $worksheets[0];
+    $listFeed = $worksheet->getListFeed();
+    $cellFeed = $worksheet->getCellFeed();
+    $cellFeed->editCell(1,1, "Name");
+    $cellFeed->editCell(1,2, "Followers");
 
 
-	$spreadsheet = $spreadsheetFeed->getByTitle('Twitter Bot');
+    $listFeed->insert([
+      'name' => $name,
+      'followers' => $followers
+    ]);
+  }
+  
+  public function enqueueStatus($status)
+  {
 
-	$worksheets = $spreadsheet->getWorksheetFeed()->getEntries();
+    $data = json_decode($status, true);
+    if (is_array($data) && isset($data['user']['screen_name'])) {
+      $this->saveInPreadSheet($data['user']['screen_name'], $data['user']['followers_count']);
+      print "Name of Profile: ".$data['user']['screen_name'] . '    Number of Followers: ' . $data['user']['followers_count'] . "\n";
+      
+    }
+  }
+}
 
-	$worksheet = $worksheets[0];
-	$listFeed = $worksheet->getListFeed();
-	$cellFeed = $worksheet->getCellFeed();
-	$cellFeed->editCell(1,1, "Name");
-	$cellFeed->editCell(1,2, "Followers");
-
-	
-
-	$data = $connection->get("search/tweets", ["q" => $hashtag, "count" => 2]);
-	
-
-	$object = $data->statuses;
-
-	$items = array();
-	
-	foreach ($object as $user)
-	{
-		$listFeed->insert([
-	        'name' => $user->user->name,
-	        'followers'	=> $user->user->followers_count
-	    ]);
-		$items[] = array(
-			'Name of Profile'	=>	$user->user->name,
-			'Numbers of Followers' => number_format($user->user->followers_count, 0)
-		);
-		
-		
-
-		
-	} 
-
-	$response->table($items);
-	
-
-?>
+// The OAuth credentials you received when registering your app at Twitter
+define("TWITTER_CONSUMER_KEY", "5DRHOI34z4Eb2P5INeZnTn5dP");
+define("TWITTER_CONSUMER_SECRET", "uH10YyynLJZIbMEQk4Zq42UHSJBmXvbhT98Kmr8yRaHBJON6AU");
+define("OAUTH_TOKEN", "269931982-lvZn8v0Uf8hW9UKl2SqRFQesvWeqrb6knreHSgzw");
+define("OAUTH_SECRET", "1UGNYBESwuTHxeLgCVU8QAqdWxLpYLLqRHO2EQZuIAPUK");
 
 
 
+// Start streaming
+$sc = new ScrapTweet(OAUTH_TOKEN, OAUTH_SECRET, Phirehose::METHOD_FILTER);
+echo "Enter the hashtag below without the # sign :\n";
+$var = fread(STDIN, 80); // Read up to 80 characters or a newline
+$hashtag = "#". $var;
+
+$sc->setTrack(array($hashtag));
+$sc->consume();
